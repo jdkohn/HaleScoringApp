305ddc86b43d0996f9b9d130dc8d2f488a88e373
jdkohn <jdkohn18@gmail.com>
2015-11-30 15:30:43 -0800

    Initial Commit

---
+++ scorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scorer
+//
+//  Created by Jacob Kohn on 11/30/15.
+//  Copyright © 2015 Jacob Kohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
